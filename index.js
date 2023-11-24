b68b5490613be12e84246c61bf6c92eea85d8f98
amiribnjaman <amiribnjaman@gmail.com>
2023-11-24 22:41:38 +0600

    Initial api endpoint implemented

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require("express")
+const app = express()
+const cors = require('cors')
+require("dotenv").config();
+const PORT = process.env.PORT || 4000
+
+
+// Middlewares
+app.use(cors())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+
+// Testing route
+app.get('/', (req, res) => {
+    res.status(200).json({
+        message: 'All right'
+    })
+})
+
+// Handling url error
+app.use((req, res, next)=> {
+    res.status(404).json({
+        message: 'Bad request'
+    })
+})
+
+// Handling server error
+app.use((error, req, res, next) => {
+    res.status(500).json({
+        message: "Internal problem"
+    })
+})
+
+
+app.listen(PORT, () => {
+  console.log(`App is running on http://localhost:${PORT}`);
+});
