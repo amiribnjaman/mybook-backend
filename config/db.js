1f34b76a671b9c994c15425e775bd859075d4b86
amiribnjaman <amiribnjaman@gmail.com>
2023-11-24 22:46:22 +0600

    config/db file created

---
+++ config/db.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+const dbURL = process.env.DB_URL;
+
+mongoose
+  .connect(dbURL)
+  .then(() => {
+    console.log("Mongodb atlas is connected");
+  })
+  .catch((error) => {
+    console.log(error);
+    process.exit(1);
+  });
+
+module.exports = mongoose;
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express")
 const app = express()
 const cors = require('cors')
 require("dotenv").config();
+require('./config/db')
 const PORT = process.env.PORT || 4000
 
 
