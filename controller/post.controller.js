e8e7bd140a44a4dae14304c45612b21b78d1596b
amiribnjaman <amiribnjaman@gmail.com>
2023-11-26 19:11:00 +0600

    Create comment api implemented

--- controller/post.controller.js
+++ controller/post.controller.js
@@ -2,23 +2,26 @@ const User = require("../model/user.model");
 const Post = require("../model/post.model");
 const { v4: uuidv4 } = require("uuid");
 
-
 // Create a new post
 const createPost = async (req, res) => {
   const { post, imgUrl } = req.body;
   try {
     // const getUser = await User.findOne({ email: email });
     // if (getUser) {
-      const newPost = new Post({
-        id: uuidv4(),
-        post,
-        userName: 'Amir Hosen',
-        userEmail: 'amir@mail.com',
-        imgUrl,
-      });
-      await newPost.save();
-      res.send({ status: 201, message: 'Post created succeffullly.', data: newPost });
-    // } 
+    const newPost = new Post({
+      id: uuidv4(),
+      post,
+      userName: "Amir Hosen",
+      userEmail: "amir@mail.com",
+      imgUrl,
+    });
+    await newPost.save();
+    res.send({
+      status: 201,
+      message: "Post created succeffullly.",
+      data: newPost,
+    });
+    // }
     // else {
     //   res.send({ status: 400, message: "Something went wrong" });
     // }
@@ -30,55 +33,65 @@ const createPost = async (req, res) => {
 // Get all post
 const getAllPost = async (req, res) => {
   try {
-      const allPost = await Post.find({}).sort({createOn: -1});
-      res.send({ status: 200, data: allPost });
-    } catch (error) {
+    const allPost = await Post.find({}).sort({ createOn: -1 });
+    res.send({ status: 200, data: allPost });
+  } catch (error) {
     res.status(500).send(error.message);
   }
 };
-
 
 // Update a post
 const updatePost = async (req, res) => {
-    const { post } = req.body;
-    const params = req.params
+  const { post } = req.body;
+  const params = req.params;
   try {
-      const check = await Post.find({ id: params });
-      if (check.userEmail == email) {
-          
-        res.send({ status: 200, data: allPost });
-      } 
-    } catch (error) {
+    const check = await Post.find({ id: params });
+    if (check.userEmail == email) {
+      res.send({ status: 200, data: allPost });
+    }
+  } catch (error) {
     res.status(500).send(error.message);
   }
 };
 
-// Update a post
-const comment = async (req, res) => {
-    const { comment } = req.body;
-    const params = req.params
+// Create a new comment 
+const createComment = async (req, res) => {
+  const { postId, comment, userId } = req.body;
   try {
-      const check = await Post.find({ id: params });
-      
-        res.send({ status: 200, data: allPost });
-    } catch (error) {
+    const post = await Post.find({ id: postId });
+    const comments = await Post.updateOne(
+      { id: postId },
+      {
+        $set: {
+          comments: [
+            ...post[0].comments,
+            {
+              id: uuidv4(),
+              postId,
+              userId,
+              comment,
+            },
+          ],
+        },
+      }
+    );
+    res.send({ status: 200, data: comments });
+  } catch (error) {
     res.status(500).send(error.message);
   }
 };
 
 // Delete a post
 const deletePost = async (req, res) => {
-    const params = req.params
+  const params = req.params;
   try {
-      const check = await Post.find({ id: params });
-      if (check.userEmail == email) {
-          
-        res.send({ status: 200, data: allPost });
-      } 
-    } catch (error) {
+    const check = await Post.find({ id: params });
+    if (check.userEmail == email) {
+      res.send({ status: 200, data: allPost });
+    }
+  } catch (error) {
     res.status(500).send(error.message);
   }
 };
 
-
-module.exports = { createPost, getAllPost };
+module.exports = { createPost, getAllPost, createComment };
--- route/post.router.js
+++ route/post.router.js
@@ -1,10 +1,11 @@
 const express = require("express");
-const { createPost, getAllPost } = require("../controller/post.controller");
+const { createPost, getAllPost, createComment } = require("../controller/post.controller");
 const router = express.Router();
 
 
 // Router
 router.post("/create", createPost);
 router.get("/allpost", getAllPost);
+router.patch("/createComment", createComment);
 
 module.exports = router;
